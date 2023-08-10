0106f4d4d61b3a876e45b49cf1d29e19df3b3175
Chamano Ramashia <chamano.sr@gmail.com>
2023-08-10 21:16:52 +0200

    adding login and sign up forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Login from './Login'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Signup from './Signup'
 
 function App() {
   return (
     <div>
-      <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}></Route>
+          <Route path='/signup' element={<Signup/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,21 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
         <div className='bg-white p-3 rounded w-25'>
+        <h2>Log In</h2>
             <form action=''>
                 <div className='mb-3'>
-                    <label htmlFor="email">Email</label>
-                    <input type='email' placeholder='Enter Email'/>
+                    <label htmlFor="email"><strong>Email</strong> </label>
+                    <input type='email' placeholder='Enter Email' className='form-control rounded-0'/>
                 </div>
                 <div className='mb-3'>
-                   <label htmlFor="password">password</label>
-                    <input type='password'  placeholder='Enter Password'/>
+                   <label htmlFor="password"><strong>Password</strong> </label>
+                    <input type='password'  placeholder='Enter Password' className='form-control rounded-0'/>
                 </div>
-                <button className='btn btn-success'>Log In</button>
+                <button className='btn btn-success w-100 rounded-0'>Log In</button>
                 <p>You agree to our terms and conditions</p>
-                <button className='btn btn-default border'>Create Account</button>
+                <Link to='/signup' className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
             </form>
         </div>
     </div>
